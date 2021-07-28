c229efaa8d26745ec031f5f376cb7c3f7b0c19f3
Renebps <renebps72@gmail.com>
2021-07-27 21:30:45 -0600

    Integrales solucionadas y sin conflicto

--- integrales.py
+++ integrales.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 print("esto es una integral que me gusta mucho")
-=======
-print("esto es una integral muy chula")
 print("Esto va a entrar en conflicto")
->>>>>>> integrales-modificadas
+
